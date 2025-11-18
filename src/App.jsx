fa93ad83e7cac1c135af06f285bbbf19c30e2139
ARYANPATEL-BIT <aryan.patel7291@gmail.com>
2025-11-18 23:44:30 +0530

    Updated update.txt

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    
-    </>
-  )
+  
 }
 
 export default App
