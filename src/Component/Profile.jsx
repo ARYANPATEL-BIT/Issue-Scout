915484e2fa698cf09299072ee59814b3de6e697c
ARYANPATEL-BIT <aryan.patel7291@gmail.com>
2025-11-21 12:02:41 +0530

    Feat-Profile Section

--- src/Component/Profile.jsx
+++ src/Component/Profile.jsx
@@ -3,12 +3,16 @@
 function Profile(){
 
     return(
-        <div className="profile bg-#">
-          <div className="profile-pic">
-            <img className="" src="" alt="" />
+        <div className="profile">
+          <div className="profile-section">
+            <div className="profile-pic">
+               <img className="picture" src="https://media.istockphoto.com/id/1327592506/vector/default-avatar-photo-placeholder-icon-grey-profile-picture-business-man.jpg?s=612x612&w=0&k=20&c=BpR0FVaEa5F24GIw7K8nMWiiGmbb8qmhfkpXcp1dhQg=" alt="" />
+            </div>
+            <div className="details">
+              <div className="user-name">Aryan Patel</div>
+              <div className="user-email">aryan.patel@example.com</div>
+            </div>
           </div>
-          <div className="user-name">Aryan Patel</div>
-          <div className="user-email">aryan.patel@example.com</div>
         </div>
     )
 
