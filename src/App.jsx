b12fd8c02d484422c820c2a331c07472db7b5c09
ARYANPATEL-BIT <aryan.patel7291@gmail.com>
2025-11-23 01:09:49 +0530

    Fixed- Search Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Profile from './Component/Profile'
 import Navbar  from './Component/Navbar'
 import Search from './Component/Search'
-import Card from './Component/Card'
 
 function App() {
   return(
